72437d19d9a0f08eae2d10f1bc514f8fa3e039f2
isthatagrapefrut <wkerr1996@gmail.com>
2018-08-19 17:25:11 +1000

    Format timestamps using moment.js

---
+++ playground/time.js
@@ -0,0 +1,18 @@
+var moment = require('moment');
+
+/*
+var date = new Date();
+console.log(date.getMonth());*/
+/*
+var date = moment();
+
+date.add(1, 'year').subtract(9, 'month');
+console.log(date.format('MMM Do, YYYY'));*/
+
+var someTimeStamp = moment().valueOf();
+console.log(someTimeStamp);
+
+var createdAt = 1234;
+var date = moment(createdAt);
+
+console.log(date.format('h:mm A'));
