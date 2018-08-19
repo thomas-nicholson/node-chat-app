fde02d90685ba84bff0b35d24c540fc097c909f3
isthatagrapefrut <wkerr1996@gmail.com>
2018-08-19 12:38:33 +1000

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,13 @@ socket.on('newLocationMessage', function(message) {
 jQuery('#message-form').on('submit', function(evt) {
   evt.preventDefault();
 
+  var messageBox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
-  }, function(data) {
-    console.log(data);
+    text: messageBox.val()
+  }, function() {
+    messageBox.val('');
   });
 });
 
@@ -44,12 +46,16 @@ locationButton.on('click',function() {
     return alert('Geolocation not supported by your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location');
+
   navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
